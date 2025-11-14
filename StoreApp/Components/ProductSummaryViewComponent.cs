6fbcc9ec5ab9e87b1c6eae044d9932c28e89f6e3
Ibrahim Parlak <ibrahimparlak3694@gmail.com>
2025-11-14 23:37:58 +0300

    ViewComponent kullanımı

---
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.EFCore;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent:ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummaryViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return _manager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+
+    }
+}
